320dd51b6b6af78e465cbe54a767e31f0b9411d5
rathan.g <rathan.g@infanion.com>
2026-08-02 11:23:13 +0530

    feat: add application logging

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 
 from app.core.config import settings
+from app.core.logger import logger
 
 app = FastAPI(
     title=settings.APP_NAME,
@@ -8,10 +9,16 @@ app = FastAPI(
 )
 
 
+@app.on_event("startup")
+async def startup():
+    logger.info("Application Started")
+
+
 @app.get("/")
 async def home():
+    logger.info("Home endpoint called")
+
     return {
         "app": settings.APP_NAME,
         "model": settings.MODEL_NAME,
-        "version": settings.APP_VERSION,
     }
